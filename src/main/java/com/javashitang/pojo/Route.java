de487a56707cad66c1006dd2fe3053491cdc83e5
lilimin <290059742@qq.com>
2020-06-10 19:36:13 +0800

    简化分组校验的demo

--- src/main/java/com/javashitang/pojo/Route.java
+++ src/main/java/com/javashitang/pojo/Route.java
@@ -18,29 +18,23 @@ public class Route {
 
     @NotNull(groups = {RouteValidStart.class}, message = "始发地省id不能为空")
     private Integer startProvinceId;
-    @NotNull(groups = {RouteValidStart.class}, message = "始发地市id不能为空")
-    private Integer startCityId;
     @NotNull(groups = {RouteValidEnd.class}, message = "目的地省id不能为空")
     private Integer endProvinceId;
-    @NotNull(groups = {RouteValidEnd.class}, message = "目的地市id不能为空")
-    private Integer endCityId;
-    @NotNull(groups = {RouteValidStart.class, RouteValidEnd.class}, message = "始发地详细地址不能为空")
-    private String startAddress;
-    @NotNull(groups = {RouteValidStart.class, RouteValidEnd.class}, message = "目的地详细地址不能为空")
-    private String endAddress;
+    @NotNull(groups = {RouteValidStart.class, RouteValidEnd.class}, message = "详细地址不能为空")
+    private String address;
 
     public static void main(String[] args) {
         Route route = Route.builder().build();
         String errorMsg = CommonUtil.getErrorResult(route, RouteValidStart.class);
-        // 目的地详细地址不能为空 始发地详细地址不能为空 始发地市id不能为空 始发地省id不能为空
+        // 始发地省id不能为空 详细地址不能为空
         System.out.println(errorMsg);
-        route = Route.builder().startProvinceId(1).startCityId(1).startAddress("始发地详细地址").build();
+        route = Route.builder().startProvinceId(1).address("详细地址").build();
         errorMsg = CommonUtil.getErrorResult(route, RouteValidStart.class);
-        // 目的地详细地址不能为空
+        // ""
         System.out.println(errorMsg);
-        route = Route.builder().startAddress("始发地详细地址").endAddress("目的地详细地址").build();
+        route = Route.builder().address("详细地址").build();
         errorMsg = CommonUtil.getErrorResult(route, RouteValidEnd.class);
-        // 目的地省id不能为空 目的地市id不能为空
+        // 目的地省id不能为空
         System.out.println(errorMsg);
     }
 
