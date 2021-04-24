99fd134ea09bf31f5cccd21c919ad331e97d25b5
lilimin <290059742@qq.com>
2021-04-24 20:30:30 +0800

    完善demo

--- src/main/java/com/javashitang/controller/RouteController.java
+++ src/main/java/com/javashitang/controller/RouteController.java
@@ -1,13 +1,15 @@
 package com.javashitang.controller;
 
 import com.javashitang.common.ServerResponse;
-import com.javashitang.groupValidate.ValidateGroup;
 import com.javashitang.groupValidate.ValidateGroup.RouteValidStart;
 import com.javashitang.pojo.Route;
+import com.javashitang.util.CommonUtil;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +23,22 @@ import java.util.List;
 public class RouteController {
 
     @RequestMapping("addRoute")
-    public ServerResponse addRoute(@Validated({RouteValidStart.class}) Route route, BindingResult bindingResult) {
+    public ServerResponse addRoute(@RequestBody @Validated({RouteValidStart.class}) Route route, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             List<FieldError> errorList = bindingResult.getFieldErrors();
             if (CollectionUtils.isNotEmpty(errorList)) {
                 return ServerResponse.illegalArgument(errorList.get(0).getDefaultMessage());
             }
+        }
+        // 调用service
+        return ServerResponse.success();
+    }
+
+    @RequestMapping("addRouteV2")
+    public ServerResponse addRouteV2(@RequestBody Route route) {
+        String errorMsg = CommonUtil.getErrorResult(route, RouteValidStart.class);
+        if (StringUtils.isNotEmpty(errorMsg)) {
+            return ServerResponse.illegalArgument(errorMsg);
         }
         // 调用service
         return ServerResponse.success();
--- src/main/java/com/javashitang/controller/UserController.java
+++ src/main/java/com/javashitang/controller/UserController.java
@@ -2,15 +2,18 @@ package com.javashitang.controller;
 
 import com.javashitang.common.ServerResponse;
 import com.javashitang.pojo.Student;
+import com.javashitang.util.CommonUtil;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -31,13 +34,38 @@ public class UserController {
         return ServerResponse.success();
     }
 
-    @RequestMapping("regist1")
-    public ServerResponse regist1(@Validated Student student, BindingResult bindingResult) {
+    @RequestMapping("registV2")
+    public ServerResponse registV2(@Valid @RequestBody Student student, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             List<FieldError> errorList = bindingResult.getFieldErrors();
             if (CollectionUtils.isNotEmpty(errorList)) {
                 return ServerResponse.illegalArgument(errorList.get(0).getDefaultMessage());
             }
+        }
+        // 调用service
+        return ServerResponse.success();
+    }
+
+    /**
+     * @Validated 比 @Valid 更强大，支持嵌套校验，分组校验
+     */
+    @RequestMapping("registV3")
+    public ServerResponse registV3(@Validated @RequestBody Student student, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> errorList = bindingResult.getFieldErrors();
+            if (CollectionUtils.isNotEmpty(errorList)) {
+                return ServerResponse.illegalArgument(errorList.get(0).getDefaultMessage());
+            }
+        }
+        // 调用service
+        return ServerResponse.success();
+    }
+
+    @RequestMapping("registV4")
+    public ServerResponse registV4(@Validated @RequestBody Student student) {
+        String errorMsg = CommonUtil.getErrorResult(student);
+        if (StringUtils.isNotEmpty(errorMsg)) {
+            return ServerResponse.illegalArgument(errorMsg);
         }
         // 调用service
         return ServerResponse.success();
